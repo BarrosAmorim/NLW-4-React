4f3f00521c732b529d9c1b56cd7f9bc144b7e15a
BarrosAmorim <barrosamorimd@gmail.com>
2021-02-22 13:03:44 -0300

    aula1

---
+++ moveit/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {ExperienceBar} from './components/ExperienceBar'
+
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
