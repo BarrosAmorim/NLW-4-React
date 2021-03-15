7dad83d8b4bcddb0e3769fd0756df4d893824fc3
BarrosAmorim <barrosamorimd@gmail.com>
2021-03-15 09:56:39 -0300

    finished component Contdown

--- moveit-next/src/components/Countdown/Countdown.tsx
+++ moveit-next/src/components/Countdown/Countdown.tsx
@@ -4,7 +4,7 @@ import style from './Countdown.module.css'
 let countdownTimeout: NodeJS.Timeout
 
 export function Countdown() {
-    const [time, setTime] = useState(25 * 60)
+    const [time, setTime] = useState(0.1 * 60)
     const [isActive, setisActive] = useState(false)
     const [hasFinished, setHasFiniched] = useState(false)
 
